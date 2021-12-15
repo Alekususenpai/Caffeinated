dee558d690f41c3bda2970a29d0951f9a0877d8d
Alekususenpai <83718750+Alekususenpai@users.noreply.github.com>
2021-12-15 20:44:19 +0100

    added email from props

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,7 +6,7 @@ export const Card = (props) => {
     <div className="card-container">
       <img alt="monster" src={`https://robohash.org/${props.monster.id}`} />
       <h2>{props.monster.name}</h2>
-      <p>{props.monster.mail}</p>
+      <p>{props.monster.email}</p>
     </div>
   );
 };
