13375174e914c1bf6aa98a6cb842eaee88610438
Alekususenpai <83718750+Alekususenpai@users.noreply.github.com>
2022-02-26 23:54:37 +0100

    card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,20 @@
 import React from "react";
+import "./Card.css";
 
 function Card({ coffee }) {
   return (
-    <div>
-      <h2>{coffee.title}</h2>
-      <p>{coffee.description}</p>
-      <ol>
-        {coffee.ingredients.map((ingredients) => (
-          <li>{ingredients}</li>
-        ))}
-      </ol>
-    </div>
+      <div className="card">
+        <div className="image">image should be her ok lets' seeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeszdfbsfbdsfbssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssee</div>
+        <div className="content">
+          <h2>{coffee.title}</h2>
+          <p>{coffee.description}</p>
+          <ol>
+            {coffee.ingredients.map((ingredients) => (
+              <li>{ingredients}</li>
+            ))}
+          </ol>
+        </div>
+      </div>
   );
 }
 
